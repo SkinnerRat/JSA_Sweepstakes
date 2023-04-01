5ec7d50c8c1c543617549621365fee83e580dc67
Jason Lee <jaeho52lee@gmail.com>
2023-03-31 21:00:04 -0700

    no sandbox option added to puppeteer

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post("/public/page.html/", async (req, res) => {
 
 async function calc(url, isWinCon) {
     const browser = await puppeteer.launch({
-        headless: false, 
+        args: ['--no-sandbox', '--disable-setuid-sandbox'], // needed to run chromium in browser
         userDataDir: "./user_data"
     }); 
     const page = await browser.newPage(); 
